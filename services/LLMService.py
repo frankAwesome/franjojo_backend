376c0fd38da5a3d661ee2dfb0323c112822f8fea
Francois <francoisdk@bbd.co.za>
2025-08-22 22:39:48 +0200

    update

--- services/LLMService.py
+++ services/LLMService.py
@@ -1,8 +1,6 @@
 import os
 
-from langchain.chains.llm import LLMChain
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
 
 
 llm = ChatOpenAI(model="gpt-5-mini", api_key=os.environ.get('OPENAI_API_KEY'))
